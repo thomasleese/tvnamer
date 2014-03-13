42065429ddcdbb6f3f1839ce27b34f0c5c4492e6
Tom Leese <tom@tomleese.me.uk>
2014-03-13 09:20:53 +0000

    Check filenames are not empty before renaming

--- src/seasonwidget.cpp
+++ src/seasonwidget.cpp
@@ -22,7 +22,7 @@ QString findBestSeasonFile(QDir dir, Season season) {
             return info.absoluteFilePath();
         }
     }
-    
+
     return QString();
 }
 
@@ -31,9 +31,9 @@ QString findBestEpisodeFile(QDir dir, Episode episode) {
     filters << "*.avi" << "*.mkv" << "*.m4v" << "*.mp4" << "*.webm" << "*.ogg"
             << "*.ogv" << "*.mpg" << "*.wmv" << "*.vob" << "*.flv" << "*.mov"
             << "*.rm" << "*.swf" << "*.3gp" << "*.wtv";
-    
+
     QFileInfoList list = dir.entryInfoList(filters, QDir::Files | QDir::NoDotAndDotDot);
-    
+
     QString num = pad(episode.num);
     QStringList prospectiveFiles;
 
@@ -43,21 +43,21 @@ QString findBestEpisodeFile(QDir dir, Episode episode) {
             return info.absoluteFilePath(); // this is clearly a proper match
         }
     }
-    
+
     // File.S06E01
     foreach (QFileInfo info, list) {
         if (info.fileName().toLower().contains("e" + num)) {
             prospectiveFiles << info.absoluteFilePath();
         }
     }
-    
+
     // File.06x01
     foreach (QFileInfo info, list) {
         if (info.fileName().contains(num)) {
             prospectiveFiles << info.absoluteFilePath();
         }
     }
-    
+
     // File.6x1
     foreach (QFileInfo info, list) {
         if (info.fileName().contains(QString::number(episode.num))) {
@@ -87,12 +87,12 @@ SeasonWidget::SeasonWidget(Season season, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SeasonWidget) {
     ui->setupUi(this);
-    
+
     if (!season.show.autoDir.isNull()) {
         QDir autoDir(season.show.autoDir);
         mDir = findBestSeasonFile(autoDir, season);
     }
-    
+
     mDatabase = new TheTVDB(this);
     mDatabase->getEpisodes(season);
     connect(mDatabase, SIGNAL(foundEpisodes(QList<Episode>)), this, SLOT(on_mDatabase_foundEpisodes(QList<Episode>)));
@@ -105,17 +105,17 @@ SeasonWidget::~SeasonWidget() {
 void SeasonWidget::saveChanges() {
     for (int i = 0; i < ui->treeEpisodes->topLevelItemCount(); i++) {
         QTreeWidgetItem *item = ui->treeEpisodes->topLevelItem(i);
-        
+
         QString oldFilename = item->text(1);
         QString newFilename = item->text(2);
-        
-        if (!oldFilename.isNull() && !newFilename.isNull() && oldFilename != newFilename) {
+
+        if (!oldFilename.isEmpty() && !newFilename.isEmpty() && oldFilename != newFilename) {
             qDebug() << "renaming" << oldFilename << "to" << newFilename;
             if (!QFile(oldFilename).rename(newFilename)) {
                 qDebug() << "failed to rename" << oldFilename << "to" << newFilename;
             }
         }
-        
+
         ui->textDirectory->setText(ui->textDirectory->text());
     }
 }
@@ -135,7 +135,7 @@ void SeasonWidget::on_btnBrowse_clicked() {
 
 void SeasonWidget::on_textDirectory_textChanged(const QString &path) {
     ui->treeEpisodes->model()->removeRows(0, ui->treeEpisodes->topLevelItemCount());
-    
+
     QDir dir(path);
     foreach (Episode episode, mEpisodes) {
         QString oldFilename = findBestEpisodeFile(dir, episode);
@@ -149,7 +149,7 @@ void SeasonWidget::on_textDirectory_textChanged(const QString &path) {
             QString ext = oldFilename.right(oldFilename.length() - oldFilename.lastIndexOf("."));
             QString num = episode.num < 10 ? "0" + QString::number(episode.num) : QString::number(episode.num);
             QString newFilename = dir.filePath(num + " - " + episode.name + ext);
-            
+
             item = new QTreeWidgetItem(QStringList() << pad(episode.num) + " - " + episode.name << oldFilename << newFilename);
             if (newFilename == oldFilename) {
                 item->setTextColor(1, QColor("green"));
@@ -163,7 +163,7 @@ void SeasonWidget::on_textDirectory_textChanged(const QString &path) {
         item->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled);
         ui->treeEpisodes->addTopLevelItem(item);
     }
-    
+
     ui->treeEpisodes->resizeColumnToContents(0);
     ui->treeEpisodes->resizeColumnToContents(1);
     ui->treeEpisodes->resizeColumnToContents(2);
