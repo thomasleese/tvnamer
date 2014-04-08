dbc7a88563c7130086f9aaf0d5faae02e4016fcb
Tom Leese <tom@tomleese.me.uk>
2014-04-08 09:28:03 +0100

    Add d-pointer for FindShowDialogue

--- src/findshowdialogue.cpp
+++ src/findshowdialogue.cpp
@@ -4,46 +4,55 @@
 #include "findshowdialogue.h"
 #include "ui_findshowdialogue.h"
 
+
+class FindShowDialoguePrivate {
+public:
+    TheTVDB *database;
+    QList<Show> shows;
+};
+
+
 FindShowDialogue::FindShowDialogue(QWidget *parent) :
     QDialog(parent),
+    d(new FindShowDialoguePrivate()),
     ui(new Ui::FindShowDialogue) {
     ui->setupUi(this);
-    
+
     connect(this, SIGNAL(accepted()), this, SLOT(on_FindShowDialogue_accepted()));
-    
-    mDatabase = new TheTVDB(this);
-    connect(mDatabase, SIGNAL(foundShows(int,QList<Show>)), this, SLOT(on_mDatabase_foundShows(int,QList<Show>)));
+
+    d->database = new TheTVDB(this);
+    connect(d->database, SIGNAL(foundShows(int,QList<Show>)), this, SLOT(on_mDatabase_foundShows(int,QList<Show>)));
 }
 
 FindShowDialogue::~FindShowDialogue() {
     delete ui;
-    delete mDatabase;
+    delete d->database;
 }
 
 Show FindShowDialogue::getCurrentShow() const {
-    Show show = mShows[ui->listShows->currentRow()];
+    Show show = d->shows[ui->listShows->currentRow()];
     show.autoDir = ui->textAutoDir->text();
     return show;
 }
 
 void FindShowDialogue::on_mDatabase_foundShows(int id, const QList<Show> &shows) {
     Q_UNUSED(id)
-    
-    mShows = shows;
-    
+
+    d->shows = shows;
+
     ui->listShows->clear();
-    foreach (Show show, mShows) {
+    foreach (Show show, d->shows) {
         ui->listShows->addItem(show.name);
     }
 }
 
 void FindShowDialogue::on_textShow_textEdited(const QString &text) {
-    mDatabase->searchShows(text);
+    d->database->searchShows(text);
 }
 
 void FindShowDialogue::on_listShows_doubleClicked(const QModelIndex &index) {
     Q_UNUSED(index)
-    
+
     accept();
 }
 
--- src/findshowdialogue.h
+++ src/findshowdialogue.h
@@ -4,7 +4,6 @@
 #include <QDialog>
 
 struct Show;
-class TheTVDB;
 
 class QModelIndex;
 
@@ -12,28 +11,26 @@ namespace Ui {
     class FindShowDialogue;
 }
 
-class FindShowDialogue : public QDialog
-{
+class FindShowDialogue : public QDialog {
     Q_OBJECT
-    
+
 public:
     explicit FindShowDialogue(QWidget *parent = 0);
     ~FindShowDialogue();
-    
+
     Show getCurrentShow() const;
-    
+
 private slots:
     void on_mDatabase_foundShows(int id, const QList<Show> &shows);
     void on_textShow_textEdited(const QString &arg1);
     void on_listShows_doubleClicked(const QModelIndex &index);
     void on_FindShowDialogue_accepted();
     void on_btnBrowse_clicked();
-    
+
 private:
     Ui::FindShowDialogue *ui;
-    TheTVDB *mDatabase;
-    QList<Show> mShows;
-    
+    class FindShowDialoguePrivate *d;
+
 };
 
 #endif // FINDSHOWSDIALOGUE_H
