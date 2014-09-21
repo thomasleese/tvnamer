5695993cb6e9ce1a69997506db0a0c18f5e4d196
Tom Leese <tom@tomleese.me.uk>
2014-09-21 15:39:37 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+with open("README.rst") as fd:
+    long_description = fd.read()
+
+setup(
+    name="tvnamer",
+    version="1.0.0-dev",
+    description="Utility to rename lots of TV video files using the TheTVDB.",
+    long_description=long_description,
+    author="Tom Leese",
+    author_email="tom@tomleese.me.uk",
+    url="https://github.com/tomleese/tvnamer",
+    packages=["tvnamer"],
+    install_requires=[
+        "pytvdbapi"
+    ],
+    classifiers=[
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Video",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
+    ]
+)
