5a6094bc84a5a17ff5792f3e1fd54df9fe3681f5
Tom Leese <inbox@tomleese.me.uk>
2014-10-23 15:00:56 +0100

    Remove 'include_extensions' from renamer
    This will be handled by the regular expression matching.

--- tvnamer/renamer.py
+++ tvnamer/renamer.py
@@ -5,13 +5,11 @@ import pytvdbapi.api as tvdb
 
 
 class Renamer:
-    include_extensions = [".avi"]
-
     def __init__(self, api_key):
         self.tvdb = tvdb.TVDB(api_key)
 
     @staticmethod
-    def flat_file_list(directory, include_extensions=None):
+    def flat_file_list(directory):
         directory = os.path.normpath(directory)
 
         for dirpath, dirnames, filenames in os.walk(directory):
@@ -19,16 +17,12 @@ class Renamer:
                 full_path = os.path.join(dirpath, filename)
                 # remove directory from the start of the full path
                 full_path = full_path[len(directory)+1:]
-
-                if include_extensions is not None:
-                    if os.path.splitext(full_path)[1] not in include_extensions:
-                        continue
                 yield full_path
 
     def rename_table(self, directory, input_regex, output_format):
         input_pattern = re.compile(input_regex)
 
-        filenames = self.flat_file_list(directory, self.include_extensions)
+        filenames = self.flat_file_list(directory)
         for filename in filenames:
             thing = input_pattern.search(filename)
             if thing is not None:
