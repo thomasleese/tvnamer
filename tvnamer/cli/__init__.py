24c9f749c58ad41dfcdf172895b46c208582cd67
Tom Leese <inbox@tomleese.me.uk>
2014-10-23 13:40:51 +0100

    Add code to build a flat file list

--- tvnamer/cli/__init__.py
+++ tvnamer/cli/__init__.py
@@ -5,10 +5,8 @@ from tvnamer import renamer
 
 def build_argument_parser():
     parser = argparse.ArgumentParser(description="Tool to rename TV files.")
-    parser.add_argument("api_key", help="API key for TheTVDB.")
-    parser.add_argument("show", help="The name of the TV show.")
-    parser.add_argument("season", type=int, help="The season of the TV show.")
     parser.add_argument("directory", help="Directory of TV files.")
+    parser.add_argument("api_key", help="API key for TheTVDB.")
     return parser
 
 def main():
@@ -16,4 +14,4 @@ def main():
     args = parser.parse_args()
 
     r = renamer.Renamer(args.api_key)
-    r.rename(args.show, args.season, args.directory)
+    r.rename(args.directory)
--- tvnamer/renamer.py
+++ tvnamer/renamer.py
@@ -7,5 +7,18 @@ class Renamer:
     def __init__(self, api_key):
         self.tvdb = tvdb.TVDB(api_key)
 
-    def rename(self, show, season, directory):
-        pass
+    @staticmethod
+    def flat_file_list(directory):
+        directory = os.path.normpath(directory)
+
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                full_path = os.path.join(dirpath, filename)
+                # remove directory from the start of the full path
+                full_path = full_path[len(directory)+1:]
+                yield full_path
+
+    def rename(self, directory):
+        filenames = self.flat_file_list(directory)
+        for file in filenames:
+            print(file)
