c5afe2d3fd1a8289757ccfbec035333f18e6674a
Tom Leese <inbox@tomleese.me.uk>
2014-10-23 19:44:39 +0100

    Change to using a QMainWindow

--- tvnamer/gui/__init__.py
+++ tvnamer/gui/__init__.py
@@ -3,9 +3,11 @@ import sys
 from PySide import QtCore, QtGui
 
 
-class DirectoryDropWidget(QtGui.QLabel):
-    def __init__(self):
-        super().__init__()
+class VideoFileFolderDropTarget(QtGui.QLabel, QtCore.QObject):
+    dropped = QtCore.Signal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.setAcceptDrops(True)
         self.setStyleSheet("background: white;")
         self.setAlignment(QtCore.Qt.AlignCenter)
@@ -20,24 +22,28 @@ class DirectoryDropWidget(QtGui.QLabel):
 
     def dropEvent(self, e):
         url = e.mimeData().urls()[0]
-        print(url)
+        self.dropped.emit(url.path())
 
 
-class MainWindow(QtGui.QWidget):
+class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("TVNamer")
         self.setMinimumSize(512, 512)
 
-        layout = QtGui.QGridLayout()
-        layout.addWidget(DirectoryDropWidget(), 0, 0)
-        layout.setColumnStretch(0, 1)
-        layout.setRowStretch(0, 1)
-        layout.setSpacing(0)
-        layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(layout)
+        self.drop_target = VideoFileFolderDropTarget(self)
+        self.drop_target.dropped.connect(self.on_drop_target_dropped)
+
+        self.status_bar = QtGui.QStatusBar(self)
+
+        self.setCentralWidget(self.drop_target)
+        self.setStatusBar(self.status_bar)
 
         self.show()
+
+    @QtCore.Slot(str)
+    def on_drop_target_dropped(self, path):
+        print(path)
 
 
 def main():
