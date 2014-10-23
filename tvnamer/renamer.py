1282a5192034b7e6c6af90ef78878456f2bb3b4a
Tom Leese <inbox@tomleese.me.uk>
2014-10-23 13:43:47 +0100

    Add file extension filtering on renamer

--- tvnamer/renamer.py
+++ tvnamer/renamer.py
@@ -4,11 +4,13 @@ import pytvdbapi.api as tvdb
 
 
 class Renamer:
+    include_extensions = [".avi"]
+
     def __init__(self, api_key):
         self.tvdb = tvdb.TVDB(api_key)
 
     @staticmethod
-    def flat_file_list(directory):
+    def flat_file_list(directory, include_extensions=None):
         directory = os.path.normpath(directory)
 
         for dirpath, dirnames, filenames in os.walk(directory):
@@ -16,9 +18,13 @@ class Renamer:
                 full_path = os.path.join(dirpath, filename)
                 # remove directory from the start of the full path
                 full_path = full_path[len(directory)+1:]
+
+                if include_extensions is not None:
+                    if os.path.splitext(full_path)[1] not in include_extensions:
+                        continue
                 yield full_path
 
     def rename(self, directory):
-        filenames = self.flat_file_list(directory)
+        filenames = self.flat_file_list(directory, self.include_extensions)
         for file in filenames:
             print(file)
